06f833c133f48f08602d4909b6f1db814895babd
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-08-26 21:09:30 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import About from './components/About';
+import Build from './components/Build';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Know from './components/Know';
+import Navbar from './components/Navbar';
+import Use from './components/Use';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Navbar />
+      <Home />
+      <About />
+      <Know />
+      <Use />
+      <Build />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Know.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Know = () => {
+    return (
+        <div>Know</div>
+    )
+}
+
+export default Know
